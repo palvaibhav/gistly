b33cdc4ef63314caff9164dfda58b6b2049de1dd
Vaibhav Pal <vaibhavpal@Vaibhavs-MacBook-Air.local>
2025-03-12 22:11:32 +0530

    hardcoded sheet url

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 window.env = {
-   GOOGLE_SHEET_URL: "%SHEET_URL%",
+   GOOGLE_SHEET_URL: "https://script.google.com/macros/s/AKfycbznAy85PuFoWOTjpbw_5P1m1WJyfMmS0DAeKC-DR1AWMhYdU-6hIU9N_K4rF7yPMwbP/exec",
 };
